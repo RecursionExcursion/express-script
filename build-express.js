c04313db1d9449560e9f15122b3c67c10c47ec85
RecursionExcursion <rloup15@gmail.com>
2024-05-04 14:12:26 -0400

    Added lib and error handling

--- build-express.js
+++ build-express.js
@@ -5,98 +5,20 @@
  *
  * Use the command 'node <path-to-script>build-express.js' to execute the script.
  */
+const operators = require("./lib/operators");
+const handleError = require("./lib/errorHandler");
 
-const { execSync } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+const dependencies = ["express", "nodemon --save-dev"];
 
-const initializeProject = () => {
-  // Initialize a new Node.js project
-  console.log("Initializing Node.js project...");
-  execSync("npm init -y");
-};
+try {
+  operators.initializeNode();
+  operators.installDependencies(dependencies);
 
-const installExpress = () => {
-  // Install Express dependency
-  console.log("Installing Express...");
-  execSync("npm install express");
-};
+  const scriptsMap = new Map();
+  scriptsMap.set("start", "nodemon index.js");
 
-const installDependencies = () => {
-  // Install Nodemon as a development dependency
-  console.log("Installing Nodemon...");
-  execSync("npm install nodemon --save-dev");
-};
-
-const customizePackageJson = () => {
-  console.log(`Customizing package.json...`);
-
-  const propertyOrder = [
-    "name",
-    "version",
-    "description",
-    "author",
-    "license",
-    "keywords",
-    "main",
-    "type",
-    "scripts",
-    "dependencies",
-    "devDependencies",
-  ];
-
-  // Read package.json file
-  const packageJsonPath = path.resolve("package.json");
-
-  const packageJson = require(packageJsonPath);
-
-  packageJson.scripts = {
-    start: "nodemon index.js",
-  };
-
-  packageJson.type = "module";
-
-  const orderedPackageJson = {};
-
-  propertyOrder.forEach((property) => {
-    if (packageJson.hasOwnProperty(property)) {
-      orderedPackageJson[property] = packageJson[property];
-    }
-  });
-
-  // Write the modified package.json back to file
-  fs.writeFileSync(
-    packageJsonPath,
-    JSON.stringify(orderedPackageJson, null, 2)
-  );
-};
-
-const buildExpressApp = () => {
-  // Create the index.js file content
-  const expressAppContent = `
-    import express from 'express';
-    const app = express();
-
-    const PORT = process.env.PORT || 8080;
-
-    app.listen(PORT, () => {
-        console.log(\`Server is running on port \${PORT}\`);
-      });
-    `;
-
-  fs.writeFileSync("index.js", expressAppContent);
-};
-
-const main = () => {
-  // Build the application
-  initializeProject();
-  installExpress();
-  installDependencies();
-  customizePackageJson();
-  console.log("Building Express application...");
-  buildExpressApp();
-  console.log("Express application built successfully!");
-};
-
-// Run the script
-main();
+  operators.customizePackageJson(scriptsMap, true);
+  operators.buildExpressApp();
+} catch (error) {
+  handleError(error);
+}

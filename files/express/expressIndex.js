524d6377715d2b9e5e52a51e5d67a8cf3c85455a
RecursionExcursion <rloup15@gmail.com>
2024-05-17 14:32:31 -0400

    create express ts set up script

--- files/express/expressIndex.js
+++ files/express/expressIndex.js
@@ -2,11 +2,12 @@ import express from "express";
 import userRouter from "./src/user/user.js";
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
 app.use("/user", userRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port \${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
